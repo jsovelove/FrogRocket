57fe80426655da1c86c808f75dead1ffd0cfd31f
Jack Sovelove <soveloj@wwu.edu>
2024-02-15 00:07:19 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,5 @@
 import { defineConfig } from 'vite'
 
-<<<<<<< HEAD
 export default defineConfig({
   base: "/FrogRocket/"
 })
-=======
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/FrogRocket/"
-})
->>>>>>> 641e264b3398e5ae92121243fbc2b05effab19db
